3d925b0a12dc53c3309e92e68692457fd5c61092
gaji12345 <gajimax12345@gmail.com>
2024-06-25 09:01:36 +0530

    test Form data-format

--- src/CustomerServlet.java
+++ src/CustomerServlet.java
@@ -19,30 +19,30 @@ public class CustomerServlet extends HttpServlet {
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/Thogakade","root","Prabath550@");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Thogakade", "root", "Prabath550@");
             ResultSet rst = connection.prepareStatement("select * from Customer").executeQuery();
-            resp.addHeader("ABCD","GAAJI");
-            String json="[";
+            resp.addHeader("ABCD", "GAAJI");
+            String json = "[";
             while (rst.next()) {
-                String customer="{";
+                String customer = "{";
                 String id = rst.getString(1);
                 String name = rst.getString(2);
                 String address = rst.getString(3);
-               double salary = rst.getDouble(4);
+                double salary = rst.getDouble(4);
 
-                customer+="\"id\":\""+id+"\",";
-                customer+="\"name\":\""+name+"\",";
-                customer+="\"address\":\""+address+"\",";
-                customer+="\"salary\":\""+salary+"\"";
+                customer += "\"id\":\"" + id + "\",";
+                customer += "\"name\":\"" + name + "\",";
+                customer += "\"address\":\"" + address + "\",";
+                customer += "\"salary\":\"" + salary + "\"";
 
-                customer+="},";
-                json+=customer;
+                customer += "},";
+                json += customer;
 
             }
 
             resp.addHeader("Content-Type", "application/json");
-            json=json+"]";
-            resp.getWriter().print(json.substring(0,json.length()-2)+"]");
+            json = json + "]";
+            resp.getWriter().print(json.substring(0, json.length() - 2) + "]");
 
 
         } catch (ClassNotFoundException e) {
@@ -53,37 +53,50 @@ public class CustomerServlet extends HttpServlet {
     }
 
 
+
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//     String id=  req.getParameter("customerId");
+//      String name=  req.getParameter("customerName");
+//     String add=   req.getParameter("customerAddress");
+//        String salary=   req.getParameter("customerSalary");
+//
+//        System.out.println(id+" "+ name+ "  "+add+" "+salary);
+//
+//
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//            Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/Thogakade","root","Prabath550@");
+//            PreparedStatement pstm = connection.prepareStatement("insert into customer values (?,?,?,?)");
+//            pstm.setObject(1,id);
+//            pstm.setObject(2,name);
+//            pstm.setObject(3,add);
+//            pstm.setObject(4,salary);
+//            boolean b=pstm.executeUpdate()>0;
+//            PrintWriter writer = resp.getWriter();
+//            if (b){
+//                writer.write("Customer added..!");
+//            }
+//
+//
+//        } catch (ClassNotFoundException e) {
+//            throw new RuntimeException(e);
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//
+//    }
+
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-     String id=  req.getParameter("customerId");
-      String name=  req.getParameter("customerName");
-     String add=   req.getParameter("customerAddress");
-        String salary=   req.getParameter("customerSalary");
-
-        System.out.println(id+" "+ name+ "  "+add+" "+salary);
+        String customerID = req.getParameter("customerId");
+        String customerName = req.getParameter("customerName");
+        String customerAddress = req.getParameter("customerAddress");
+        String customerSalary = req.getParameter("customerSalary");
 
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/Thogakade","root","Prabath550@");
-            PreparedStatement pstm = connection.prepareStatement("insert into customer values (?,?,?,?)");
-            pstm.setObject(1,id);
-            pstm.setObject(2,name);
-            pstm.setObject(3,add);
-            pstm.setObject(4,salary);
-            boolean b=pstm.executeUpdate()>0;
-            PrintWriter writer = resp.getWriter();
-            if (b){
-                writer.write("Customer added..!");
-            }
-
-
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-
+        System.out.println(customerID+" "+customerName+" "+customerAddress+" "+customerSalary);
     }
 }
