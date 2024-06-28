9fdb4689c1d362df6a88a45fe4b241307ccfb47f
gaji12345 <gajimax12345@gmail.com>
2024-06-28 19:35:00 +0530

    doPost Implement with json

---
+++ Json1/src/CustomerServlet.java
@@ -0,0 +1,159 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+@WebServlet(urlPatterns = "/customer")
+public class CustomerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+         resp.setContentType("application/json");
+
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ezypos", "root", "Prabath550@");
+            ResultSet rst = connection.prepareStatement("select * from Customer").executeQuery();
+            resp.addHeader("ABCD", "GAAJI");
+
+            JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+
+
+
+            while (rst.next()) {
+                String customer = "{";
+                String id = rst.getString(1);
+                String name = rst.getString(2);
+                String address = rst.getString(3);
+                double salary = rst.getDouble(4);
+
+                JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                     objectBuilder.add("id", id);
+                objectBuilder.add("name", name);
+                objectBuilder.add("address", address);
+                objectBuilder.add("salary", salary);
+
+                //addd to json array
+                arrayBuilder.add(objectBuilder.build());
+
+
+
+            }
+
+            PrintWriter writer = resp.getWriter();
+            writer.print(arrayBuilder.build());
+
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("customerId");
+        String name = req.getParameter("customerName");
+        String add = req.getParameter("customerAddress");
+        String salary = req.getParameter("customerSalary");
+
+        System.out.println(id + " " + name + "  " + add + " " + salary);
+
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ezypos", "root", "Prabath550@");
+            PreparedStatement pstm = connection.prepareStatement("insert into customer values (?,?,?,?)");
+            pstm.setObject(1, id);
+            pstm.setObject(2, name);
+            pstm.setObject(3, add);
+            pstm.setObject(4, salary);
+            boolean b = pstm.executeUpdate() > 0;
+            PrintWriter writer = resp.getWriter();
+            if (b) {
+                writer.write("Customer added..!");
+            }
+
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("delete");
+//        String id = req.getParameter("customerId");//null
+        String id = req.getParameter("cusID");
+        System.out.println(id);
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Thogakade", "root", "Prabath550@");
+            PreparedStatement pstm = connection.prepareStatement("delete from customer where id=? ");
+            pstm.setObject(1, id);
+            boolean b = pstm.executeUpdate() > 0;
+            PrintWriter writer = resp.getWriter();
+            if (b) {
+                writer.write("Customer deleted..!");
+            }
+
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String cid = req.getParameter("customerId");
+        String name = req.getParameter("customerName");
+        String add = req.getParameter("customerAddress");
+        String salary = req.getParameter("customerSalary");
+
+        System.out.println(cid + " " + name + "  " + add + " " + salary);
+
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Thogakade", "root", "Prabath550@");
+            PreparedStatement pstm = connection.prepareStatement("update customer set  name=?,address=?,salary=?  where id=? ");
+
+            pstm.setObject(1, name);
+            pstm.setObject(2, add);
+            pstm.setObject(3, salary);
+            pstm.setObject(4, cid);
+            boolean b = pstm.executeUpdate() > 0;
+            PrintWriter writer = resp.getWriter();
+            if (b) {
+                writer.write("Customer Updated..!");
+            }
+
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
+
